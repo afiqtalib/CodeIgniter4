31b8600af923740d923ada069e4019b95556aa1b
Muhammad Afiq <afiqtalib2580@gmail.com>
2022-12-13 22:27:11 +0800

    komit

--- application/models/query_Model.php
+++ application/models/query_Model.php
@@ -2,4 +2,9 @@
 class query_Model extends CI_Model
 {
 
+    function update_data($where,$table) {
+        $this->db->where('location',$where);
+        $this->db->update('orders', $table);
+    }
+
 }
